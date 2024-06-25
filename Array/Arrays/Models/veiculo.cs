c25d97cd2d0cfb68ac632cb12bb900a237f611a6
MACHADO-Caetano <machadocaetano@rede.ulbra.br>
2024-06-24 21:00:03 -0300

    24/06/2024 - Alteração: Introdução a Arrays e Lists em C#

---
+++ Array/Arrays/Models/veiculo.cs
@@ -0,0 +1,13 @@
+public abstract class Veiculo
+{
+    protected int Velocidade;
+
+    public abstract void Acelerar();
+    public abstract void Frear();
+
+    public void ExibirVelocidade()
+    {
+        System.Console.WriteLine(Velocidade);
+    }
+}
+
---
+++ Array/Arrays/Program.cs
@@ -0,0 +1,46 @@
+﻿// // Sintaxe para criação de arrays
+
+// int[] notas = new int[5];
+
+// notas[0] = 90;
+// notas[1] = 85;
+// notas[2] = 88;
+// notas[3] = 92;
+// notas[4] = 95;
+
+// for(int i = 0; i < notas.Length; i++)
+// {
+//     System.Console.WriteLine($"Aluno : {i+1} nota: {notas[i]}");
+// }
+
+// // Sintaxe para criação de list
+
+// List<string> tarefas = new List<string>();
+
+// tarefas.Add("Estudar C#");
+// tarefas.Add("Ler um livro de programação");
+// tarefas.Add("Estudar inglês");
+
+// tarefas.Remove("Estudar inglês");
+
+// foreach(string t in tarefas)
+// {
+//     System.Console.WriteLine(t);
+// }
+
+using System;
+
+List<Veiculo> veiculos = new List<Veiculo>();
+Carro carro = new Carro();
+Moto moto = new Moto();
+veiculos.Add(carro);
+veiculos.Add(moto);
+
+foreach(Veiculo vehicle in veiculos)
+{
+    vehicle.Acelerar();
+    vehicle.ExibirVelocidade();
+    vehicle.Frear();
+    vehicle.ExibirVelocidade();
+}
+
